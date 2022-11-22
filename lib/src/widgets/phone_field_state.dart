31119e8f4432763edb71701bb3d20a2b440cc815
Banse Bruno <33189102+br3n9@users.noreply.github.com>
2022-11-22 02:47:31 +0000

    Update phone_field_state.dart

--- lib/src/widgets/phone_field_state.dart
+++ lib/src/widgets/phone_field_state.dart
@@ -52,6 +52,7 @@ class PhoneFieldState extends State<PhoneField> {
             decoration: _getOutterInputDecoration(),
             isFocused: controller.focusNode.hasFocus,
             isEmpty: _isEffectivelyEmpty(),
+            expands: true,
             child: TextField(
               focusNode: controller.focusNode,
               controller: controller.nationalNumberController,
